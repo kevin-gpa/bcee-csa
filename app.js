8f0083afee04c1acf925d59f393ee1bcdc0eb621
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-09-30 10:47:44 +0200

    New attempt

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const port = process.env.PORT || 3000;
 
 /**Middleware */
 app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views/pages"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
 /**Routes */
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("pages/index");
 });
 
 

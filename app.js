3ef3f0ccd0dad73d02de32df66bf5bdc386692bd
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-09-30 09:24:40 +0200

    Create EJS pages with partials

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/**Middleware */
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+
+/**Routes */
+
+app.get("/", (req, res) => {
+    res.render("pages/index.ejs");
+});
+
+
+/**Server */
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
